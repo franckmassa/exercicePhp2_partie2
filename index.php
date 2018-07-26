543e1c2be258fb0885d29324f703dad48107ba81
massa <massafranck@gmail.com>
2018-07-26 16:26:19 +0200

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$isEasy = true;
+ ?>
+ <!DOCTYPE html>
+ <html lang="fr">
+   <head>
+     <meta charset="utf-8" />
+     <title>Exercice2</title>
+   </head>
+   <body>
+     <p>
+<?php
+if ($isEasy == true){
+  echo 'C\'est facile';
+}else{
+  echo 'C\'est dificile';
+}
+ ?>
+     </p>
+   </body>
+ </html>
